bc7acc389789306a7227c6d653f468554720b45f
Misha Golovach <golovach.miha01@gmail.com>
2024-02-09 21:35:21 +0300

    step

--- gribapi/ecmwf_process.py
+++ gribapi/ecmwf_process.py
@@ -7,7 +7,7 @@ def create_ecmwf_requests(cycle, forecast_step, weather_date, date, parameters=N
         parameters = ['tp', ('10v', '10u'), '2t', 'r', 'msl']
     requests = []
     for param in parameters:
-        for i in range(0, MAX_FORECAST_STEP + 1, forecast_step):
+        for i in range(0, MAX_FORECAST_STEP + 1 if cycle in [0, 12] else MAX_FORECAST_STEP - 11, forecast_step):
             request = {
                 "time": cycle,
                 "date": weather_date,
